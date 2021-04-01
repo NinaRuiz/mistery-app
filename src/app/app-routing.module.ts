4361b0a36e854265cc99dac37d20dc2cddb2c4e2
Git Sadiel <sadiel.gutierrez.ext@concatel.com>
2021-04-01 19:38:38 +0200

    Routed notes module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {FullLayoutComponent} from './shared/layouts/full-layout/full-layout.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: FullLayoutComponent,
-    children: [
-      {
-        path: '',
-        loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule)
-      }
-    ]
+    loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule)
+  },
+  {
+    path: 'notes',
+    loadChildren: () => import('./modules/notes/notes.module').then(m => m.NotesModule)
   }
 ];
 
@@ -19,4 +17,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/modules/main/main-view/main-view.component.ts
+++ src/app/modules/main/main-view/main-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-main-view',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onOptionClick(path: string): void {
+    this.router.navigate([path]);
   }
 
 }
