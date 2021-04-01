d1ad35f15b2eec4d297fc91025edc5358512aa28
Git Sadiel <sadiel.gutierrez.ext@concatel.com>
2021-04-01 19:07:07 +0200

    Routed full layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {FullLayoutComponent} from './shared/layouts/full-layout/full-layout.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule)
+    component: FullLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule)
+      }
+    ]
   }
 ];
 
