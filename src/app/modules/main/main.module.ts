7dac586aea6b8f0cc076b4cb0a01242f89e27cdc
Git Sadiel <sadiel.gutierrez.ext@concatel.com>
2021-04-01 19:32:51 +0200

    Made main view

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainViewComponent } from './main-view/main-view.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
 
 
 @NgModule({
   declarations: [MainViewComponent],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    MatCardModule,
+    MatListModule
   ]
 })
 export class MainModule { }
