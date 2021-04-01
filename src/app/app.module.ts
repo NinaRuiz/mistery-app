5e0a69930362c0ed0be6ea25bfee0040bffa5de8
Git Sadiel <sadiel.gutierrez.ext@concatel.com>
2021-04-01 19:11:38 +0200

    Install angular material and bootstrap css.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FullLayoutComponent } from './shared/layouts/full-layout/full-layout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { FullLayoutComponent } from './shared/layouts/full-layout/full-layout.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
